7fe5e141c5c76888f0cd737da72dd139d8ee548b
Kesav Gopan <139054897+KesavGopan10@users.noreply.github.com>
2025-07-18 02:02:12 -0700

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { Analytics } from "@vercel/analytics/next"
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -12,6 +11,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
